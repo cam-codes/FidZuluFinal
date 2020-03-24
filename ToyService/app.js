274238951e50ebaf2850f74e628cc63742cc4902
Trevor Lounsbury <TLounsbury53@gmail.com>
2020-03-24 16:33:59 -0400

    added cors

--- ToyService/app.js
+++ ToyService/app.js
@@ -6,12 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var teamRouter = require('./routes/team');
+var cors = require('cors')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
